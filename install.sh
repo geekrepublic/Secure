5797514e760957719e93cd4216305dbc07c0e4ba
geekrepublic <criticalmass24@gmail.com>
2014-05-19 13:33:20 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,14 @@
 echo "Welcome, $USER"
 echo "We will now setup classification.config files"
 echo "Pulling files from GitHub"
-git clone https://github.com/geekrepublic/secure.git ~/Desktop/tmp/
+sudo git clone https://github.com/geekrepublic/secure.git ~/Desktop/tmp/
 echo "Now copying files from Desktop to correct path"
 #
-cp ~/Desktop/tmp/secure/classification.config /etc/nsm/nsdids-eth1/
-cp ~/Desktop/tmp/secure/classification.config /etc/nsm/rules/
-cp ~/Desktop/tmp/secure/classification.config /etc/nsm/pulledpork/
-echo "system will now format"
+sudo cp ~/Desktop/tmp/secure/classification.config /etc/nsm/nsdids-eth1/
+echo "File has been copied"
+sudo cp ~/Desktop/tmp/secure/classification.config /etc/nsm/rules/
+echo "File has been copied"
+sudo cp ~/Desktop/tmp/secure/classification.config /etc/nsm/pulledpork/
+echo "File has been copied"
+echo "Installation is finished"
+echo "Have a great day"
