53f121400775fe42c7c1354a910cf237db07ff09
geekrepublic <criticalmass24@gmail.com>
2014-05-20 12:50:24 -0500

    Create deletelogs.sh

---
+++ deletelogs.sh
@@ -0,0 +1,15 @@
+!/bin/bash/
+#
+echo "Welcome, $USER"
+echo "This operation will delete log files that maybe eating up to much hard drive space."
+echo "Remember to always buckle up. Its the law!"
+while true; do
+    read -p "Do You Wish To Delete Logs??? Once Logs Are Deleted They Can Not Be Restored" yn
+    case $yn in
+        [Yy]* ) sudo rm -rf /nsm/sensor_data/nsdids-eth1/dailylogs/*;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer (Y) yes or (N) no.";;
+    esac
+done
+echo "Thank you and have a great $USER"
+
