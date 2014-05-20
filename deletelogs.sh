bf58ab5a492ccb3dd0a2b9c0c5de084a61dfd023
geekrepublic <criticalmass24@gmail.com>
2014-05-20 13:04:19 -0500

    Update deletelogs.sh

--- deletelogs.sh
+++ deletelogs.sh
@@ -6,10 +6,10 @@ echo "Remember to always buckle up. Its the law!"
 while true; do
     read -p "Do You Wish To Delete Logs??? Once Logs Are Deleted They Can Not Be Restored" yn
     case $yn in
-        [Yy]* ) sudo rm -rf /nsm/sensor_data/nsdids-eth1/dailylogs/*;;
+        [Yy]* ) find /nsm/sensor_data/nsdids-eth1/dailylogs/* -mtime +30 -exec rm {} \;
         [Nn]* ) exit;;
         * ) echo "Please answer (Y) yes or (N) no.";;
     esac
 done
 echo "Thank you and have a great $USER"
-
+exit
